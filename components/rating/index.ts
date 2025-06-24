665a8ac783cfa5a932de29f2b0c7436da38a99d0
KARL-AJ <karlaj@notqwerty.com>
2025-06-24 12:38:31 +0300

    Revert "/// refactor create review and made 10 components menna and added docs ///"
    This reverts commit 4685adf97ca4bd0670df1c1396a62706b0370446.

--- components/rating/index.ts
+++
@@ -1,6 +0,0 @@
-export { UserRating } from "./UserRating";
-export { UserRatingBadge } from "./UserRatingBadge";
-export { UserRatingStats } from "./UserRatingStats";
-export { ReviewRatingStep } from "./ReviewRatingStep";
-export { DetailedRatingsStep } from "./DetailedRatingsStep";
-export { ReviewTagsStep } from "./ReviewTagsStep";
--- components/review/index.ts
+++
@@ -1,4 +0,0 @@
-export { ReviewSummary } from "./ReviewSummary";
-export { ReviewHeader } from "./ReviewHeader";
-export { ReviewPhotoUploader } from "./ReviewPhotoUploader";
-export { ReviewWriteStep } from "./ReviewWriteStep";
